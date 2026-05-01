b94b1ee2ec3134562d93bec84e6822c9201d77a8
it25102006 <it25102006@my.sliit.lk>
2026-05-01 18:49:56 +0530

    comment name

--- src/model/Booking.java
+++ src/model/Booking.java
@@ -14,6 +14,7 @@ public class Booking {
         this.customerName = customerName;
         this.eventDate = eventDate;
         this.location = location;
+        //this.Dumindu
     }
 
     public String getBookingId() {
